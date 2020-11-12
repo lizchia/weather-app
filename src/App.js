b1b269a466b30145096b25c04313910aa24061c6
lizchia <midory0cc@hotmail.com>
2020-11-12 10:00:08 +0800

    ADD ICON

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 
 import './App.css'
 
+
 function App() {
+  const [city , setCity] = useState('')
+  const [cityKey, setCityKey]= useState(0)
+  
+  const keyApi = 'UwBxLkrA0V3EjRDmR85ccshr6t9D8RhI';
+
+  async function getCity() {
+
+    const request = new Request('http://dataservice.accuweather.com/locations/v1/cities/search', {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'appliaction/json',
+      }),
+    })
+    console.log(request)
+    const query = `?apikey=${keyApi}&q=${city}`;
+    const response = await fetch(request.url + query)
+    console.log(response)
+    const data = await response.json()
+    const cities = data[0]
+    const cityKey = cities.Key
+    
+    setCityKey(cityKey)
+
+    console.log(data)
+    
+  }
+
+  async function getWeather() {
+    const request = new Request('http://dataservice.accuweather.com/currentconditions/v1/', {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'appliaction/json',
+      }),
+    })
+    
+    const query = `${cityKey}?apikey=${keyApi}`;
+    const response = await fetch(request.url + query)
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    
+  }
+  
+  useEffect((city) => {
+    getCity((city)=>{
+      getWeather(cityKey);
+    });
+    
+  }, [])
+
   return (
+    <>
     <div className="">
       <header className="App-header">
       <div class="container my-5 mx-auto">
@@ -12,16 +66,16 @@ function App() {
 
       <form class="change-location my-4 text-center text-muted">
         <label for="city">Enter a location for weather information</label>
-        <input type="text" name="city" class="form-control p-4" />
+        <input type="text" name="city" class="form-control p-4" onChange={(event)=>{console.log(event.target.value);setCity(event.target.value);console.log(city)}}/>
       </form>
 
       <div class="card shadow-lg rounded">
-        <img src="https://via.placeholder.com/400x300" class="time card-img-top" />
+        <img src="https://via.placeholder.com/400x300" class="time card-img-top" alt=''/>
         <div class="icon bg-light mx-auto text-center">
           
         </div>
         <div class="text-muted text-uppercase text-center details">
-          <h5 class="my-3">City name</h5>
+          <h5 class="my-3">{city}</h5>
           <div class="my-3">Weather condition</div>
           <div class="display-4 my-4">
             <span>temp</span>
@@ -34,6 +88,7 @@ function App() {
         
       </header>
     </div>
+    </>
   );
 }
 
